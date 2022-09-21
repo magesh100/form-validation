1ef3996fbc006a58a4dff732d47d62a7850e9960
magesh100 <mmageshofficial@gmail.com>
2022-09-21 19:00:28 +0530

    add
    add

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 'use strict';
+document.getElementById('title').textContent = ' Welcome to this Form';
+document.getElementById('title').style.color = '#5c2bbe';
+const newEl = document.createElement('h2');
+newEl.textContent = 'You can fill  this Form Here';
+newEl.style.textAlign = 'center';
+newEl.style.marginTop = 1;
+document.querySelector('.contain').appendChild(newEl);
+
+const formEl = getElementById('form');
+const usernameEl = getElementById('username');
+const surnameEl = getElementById('surname');
+const mobileEl = getElementById('mobile');
+const passwordEl      = getElementById('new-password');
+const  confirmEl   = getElementById('confirm-password');
+
+formEl.addEventListener('submit',function () {
+  alert('submitted');
+});
